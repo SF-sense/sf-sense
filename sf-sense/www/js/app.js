382dfc61ffdf60ee492a6635faa5baef7ea79bec
Matthew Olde <mattolde@gmail.com>
2014-05-28 00:35:01 -0700

    Create googleMaps service and intergrated it into the app. Currently using testing data.

--- sf-sense/www/js/app.js
+++ sf-sense/www/js/app.js
@@ -1,63 +1,94 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('sfSense', ['ionic'])
 
-.factory('GoogleMaps', function(){
+.factory('googleMaps', function(){
+  // OVERVIEW
+  // 1. setup map options
+  // 2. create map
+  // 3. get data for crime lat and lng
+  // 4. create markers
+  // 5. add markers to map  
+  var map;
+  var createMarker = function(lat, lng, title) {
+    var latlng = new google.maps.LatLng(lat,lng);
+
+    new google.maps.Marker({
+      position: latlng,
+      animation: google.maps.Animation.DROP,
+      title: title,
+      map: map
+    });
+  };
+
   return {
-    // add in google maps functions here
+
+    // Add in google maps functions here
+
+    createMap: function(lat, lng){
+      var mapOptions = {
+        center: new google.maps.LatLng(lat, lng),
+        zoom: 17
+      };
+
+      map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    },
+    createMarkers: function(markerLocs) {
+      for(var i = 0; i < markerLocs.length; i++){
+        var lat = markerLocs[i].lat;
+        var lng = markerLocs[i].lng;
+        var title = markerLocs[i].title;
+        createMarker(lat,lng,title);
+      }
+    },
+    moveTo: function(lat, lng){
+      var latlng = new google.maps.LatLng(lat,lng);
+      map.panTo(latlng);
+    }
   }
 })
 
 .controller('MapCtrl', function($scope, GoogleMaps){
 
-  var initialize = function() {
-    // create map object
-  };
-
-  $scope.searchCrime = function() {
-    // create map with google map API
-
-    // OVERVIEW
-    // 1. setup map options
-    // 2. create map
-    // 3. get data for crime lat and lng
-    // 4. create markers
-    // 5. add markers to map
-
-    // search for address and get lat and log for map creation
-    // $scope.mapSearch for user input
+  var init = function() {
+    // SF center lat and lng
     var lat = 37.783522;
     var lng = -122.408964;
 
-    // set map options
-    var mapOptions = {
-      center: new google.maps.LatLng(lat, lng),
-      zoom: 17
-    };
-
-    // create map
-    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-
-    // search for crimes in the radius of lat and lng from SFSense REST API
-
-    // create markers of google.maps.Marker
-
-    // create lat lng for marker objects
-    var myLatlng = new google.maps.LatLng(lat,lng);
-
-    // create marker object
-    var marker = new google.maps.Marker({
-      position: myLatlng,
-      title:"Hello World!"
-    });
-
-    // add marker to map
-    marker.setMap(map);
+    googleMaps.createMap(lat, lng);
   };
 
+  $scope.searchCrime = function() {
+
+    // $scope.mapSearch for user input
+    // Testing lat and long and panTo
+    var testLat = 37.783522;
+    var testLng = -122.409999;
+
+    googleMaps.moveTo(testLat, testLng);
+
+    // Testing crime locations
+    var testCrimeLocs = [
+      { 
+        lat: 37.783522,
+        lng: -122.408999,
+        title: 'test1'
+      },
+      { 
+        lat: 37.783522,
+        lng: -122.409999,
+        title: 'test2'
+      },
+      { 
+        lat: 37.783522,
+        lng: -122.409878,
+        title: 'test3'
+      }      
+    ];
+
+    // search for crimes in the radius of lat and lng from SFSense REST API
+    googleMaps.createMarkers(testCrimeLocs);
+  };
+
+  init();
 })
 
 .run(function($ionicPlatform) {
