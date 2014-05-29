d1ba01490644092a587c93b430849b68014e678e
rcpeng34 <rcpeng@gmail.com>
2014-05-29 16:00:34 -0700

    Changes server input variable name from marker to crime

--- sf-sense/www/js/app.js
+++ sf-sense/www/js/app.js
@@ -20,25 +20,25 @@ angular.module('sfSense', ['ionic'])
     'DEFAULT': 'missing.png'
   };
 
-  var createMarker = function(marker) {
-    var latlng = new google.maps.LatLng(marker.latitude,marker.longitude);
+  var createMarker = function(crime) {
+    var latlng = new google.maps.LatLng(crime.latitude,crime.longitude);
 
     var icon;
 
-    if(markerImg[marker.category]){
-      icon = iconPath + markerImg[marker.category];
+    if(markerImg[crime.category]){
+      icon = iconPath + markerImg[crime.category];
     } else {
-      icon = iconPath + markerImg['DEFAULT'];
+      icon = iconPath + markerImg.DEFAULT;
     }
 
-    new google.maps.Marker({
+    markers.push(new google.maps.Marker({
       position: latlng,
       animation: google.maps.Animation.DROP,
-      title: marker.title,
+      title: crime.title,
       icon: icon,
       map: map,
       draggable: false
-    });
+    }));
   };
 
   return {
