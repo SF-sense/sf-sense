b5fa2316bec3b949accea3a369a67f50679096e8
Matthew Olde <mattolde@gmail.com>
2014-05-29 22:47:38 -0700

    added cordova plugins dialogs and vibration

--- sf-sense/platforms/android/assets/www/cordova_plugins.js
+++ sf-sense/platforms/android/assets/www/cordova_plugins.js
@@ -13,6 +13,27 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -21,7 +42,9 @@ module.exports.metadata =
     "com.ionic.keyboard": "0.0.1",
     "org.apache.cordova.console": "0.2.8",
     "org.apache.cordova.device": "0.2.9",
-    "org.apache.cordova.geolocation": "0.3.8-dev"
+    "org.apache.cordova.geolocation": "0.3.8-dev",
+    "org.apache.cordova.vibration": "0.3.9-dev",
+    "org.apache.cordova.dialogs": "0.2.8-dev"
 }
 // BOTTOM OF METADATA
 });
--- sf-sense/platforms/ios/www/cordova_plugins.js
+++ sf-sense/platforms/ios/www/cordova_plugins.js
@@ -55,6 +55,20 @@ module.exports = [
         "clobbers": [
             "navigator.geolocation"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -63,7 +77,9 @@ module.exports.metadata =
     "com.ionic.keyboard": "0.0.1",
     "org.apache.cordova.console": "0.2.8",
     "org.apache.cordova.device": "0.2.9",
-    "org.apache.cordova.geolocation": "0.3.8-dev"
+    "org.apache.cordova.geolocation": "0.3.8-dev",
+    "org.apache.cordova.vibration": "0.3.9-dev",
+    "org.apache.cordova.dialogs": "0.2.8-dev"
 }
 // BOTTOM OF METADATA
 });
